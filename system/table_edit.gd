a798a1a2861efbcad181ba6eb1417999a4bc7d8a
Dove <1515098414@qq.com>
2025-11-29 14:12:46 +0800

    title line

--- system/table_edit.gd
+++ system/table_edit.gd
@@ -1,19 +1,38 @@
 extends Control
 class_name TableEdit
 
+@onready var titlescroller = $TitleScroller
+@onready var titleline = $TitleScroller/TitleLine
+@onready var gridscroller = $ScrollContainer
 @onready var grid = $ScrollContainer/Grid
+
+var data
 
 func _ready():
 	var skills = ResourceLoader.load("res://resources/skills.csv")
 	print("data in the table: ", skills.records)
+	data = skills.records
 	var max_column = 0
-	for row in skills.records:
+	var row_index = 0
+	for row in data:
 		for cell in row:
-			var celledit = LineEdit.new()
+			var celledit = RichTextLabel.new()
+			celledit.scroll_active = false
 			celledit.text = "%s" % cell
-			celledit.custom_minimum_size = Vector2(120, 32)
-			grid.add_child(celledit)
+			celledit.bbcode_enabled = true
+			celledit.custom_minimum_size = Vector2(150, 32)
+			if row_index == 0:
+				titleline.add_child(celledit)
+				celledit.text = ""
+				celledit.push_bold()
+				celledit.append_text("%s" % cell)
+				celledit.pop()
+			else:
+				grid.add_child(celledit)
 		if row.size() > max_column:
 			max_column = row.size()
+		row_index += 1
 	grid.columns = max_column
-	
+
+func _process(delta):
+	titlescroller.scroll_horizontal = gridscroller.scroll_horizontal
